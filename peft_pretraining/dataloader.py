ff876771f38c8cada39b1a27389e80c5f344b861
root <kostas.perifanos@gmail.com>
2024-04-01 23:51:02 +0000

    el

--- peft_pretraining/dataloader.py
+++ peft_pretraining/dataloader.py
@@ -4,6 +4,49 @@ import torch
 from torch.utils.data import IterableDataset, get_worker_info
 
 
+class MyDataset(IterableDataset):
+    def __init__(self, data,  batch_size, max_length):
+        super().__init__()
+        self.data = data
+        self.batch_size = batch_size
+        self.max_length = max_length
+
+    def __iter__(self):
+        worker_info = get_worker_info()
+        if worker_info is None:
+            # If no worker_info is provided, we are not using DataLoader workers, so yield all data
+            iter_data = self.data
+        else:
+            # If using DataLoader workers, yield a subset of the data for this worker
+            worker_id = worker_info.id
+            num_workers = worker_info.num_workers
+            iter_data = itertools.islice(self.data, worker_id, None, num_workers)
+            iter_data = list(iter_data)
+        
+        batch = []
+        for _ in range(12313125343):
+           
+            start_pos = torch.randint( low = 0, high=len(iter_data)-self.max_length, size = (1,) )
+            end_pos = start_pos+self.max_length 
+            
+            this_batch = iter_data[start_pos.item():end_pos.item()] 
+            batch.append(  this_batch )
+
+            if len(batch) == self.batch_size:
+                yield torch.tensor(batch, dtype=torch.int64)
+                batch = []
+
+        #if batch:
+        #    yield self._format_batch(batch)
+
+    def _format_batch(self, batch):
+        input_ids = torch.stack([item["input_ids"].squeeze(0) for item in batch])
+        attention_mask = torch.stack([item["attention_mask"].squeeze(0) for item in batch])
+
+        return {"input_ids": input_ids, "attention_mask": attention_mask}
+
+
+
 class PreprocessedIterableDataset(IterableDataset):
     def __init__(self, data, tokenizer, batch_size, max_length):
         super().__init__()
@@ -16,12 +59,13 @@ class PreprocessedIterableDataset(IterableDataset):
         worker_info = get_worker_info()
         if worker_info is None:
             # If no worker_info is provided, we are not using DataLoader workers, so yield all data
-            iter_data = iter(self.data)
+            iter_data = self.data
         else:
             # If using DataLoader workers, yield a subset of the data for this worker
             worker_id = worker_info.id
             num_workers = worker_info.num_workers
             iter_data = itertools.islice(self.data, worker_id, None, num_workers)
+            iter_data = list(iter_data)
 
         batch = []
         for example in iter_data:
@@ -35,7 +79,7 @@ class PreprocessedIterableDataset(IterableDataset):
             batch.append(tokenized_example)
 
             if len(batch) == self.batch_size:
-                yield self._format_batch(batch)
+                yield torch.tensor(batch)
                 batch = []
 
         if batch:
@@ -46,3 +90,22 @@ class PreprocessedIterableDataset(IterableDataset):
         attention_mask = torch.stack([item["attention_mask"].squeeze(0) for item in batch])
 
         return {"input_ids": input_ids, "attention_mask": attention_mask}
+
+
+if __name__ ==  "__main__":
+
+    batch_size = 8
+    data = torch.load("../data/training.pt")
+    print( "data loaded...")
+    
+    dataset = MyDataset(data,  batch_size=batch_size, max_length=512)
+    dataloader = torch.utils.data.DataLoader(dataset, batch_size=None, num_workers=2)
+
+    idx=0
+    for batch in dataloader:
+        
+        print(batch[0:10])
+        idx+=1
+        if idx==100:
+            break
+
